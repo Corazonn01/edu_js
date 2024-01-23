04343ef8a20294dc27bc0c81b98231a56e8b7940
Elina Makipova <82317623+Corazonn01@users.noreply.github.com>
2024-01-23 10:27:23 +0300

    task2.js

---
+++ task2.js
@@ -0,0 +1,8 @@
+//task witch
+function tellFortune(num_children, name_partner, geo, activity) {
+    console.log("You will have " + num_children + " children. The name of the husband is " + name_partner + ", also the location is " + geo + " and you will be a " + activity)
+    console.log(num_children, name_partner, geo, activity)
+}
+tellFortune(3, "Jacob", "London", "nurse")
+tellFortune(2, "Misha", "Oslo", "businesswoman")
+tellFortune(4, "Miguel", "Mexico city", "beaty model")
